c2b5982f9ab519fed7fb262e0693035f1c0b8abb
Jack Jack <dhaynes1203@gmail.com>
2020-02-04 12:16:02 -0600

    add fix to dynamically add current class to the current thumbnail

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -4,10 +4,14 @@ function activateGallery(){
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
-      //code to set clicked image as main images
+      //code to set clicked image as main images and set alt tag
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", thumbnail.alt);
+
+      //code to set thumbnail as current
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
     });
   });
 }
