54c5574e92682c0fa7099f2aeca5e0871c64e862
Jack Jack <dhaynes1203@gmail.com>
2020-02-04 17:32:17 -0600

    add fix for alt tags/ pre-loaded large images and gallery info match up

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,33 @@
-function activateGallery(){
+function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-  let mainImage  = document.querySelector("#gallery-photo > img");
+  let mainImage = document.querySelector("#gallery-photo > img");
 
   thumbnails.forEach(function(thumbnail) {
+    //Preload large images
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+    
     thumbnail.addEventListener("click", function() {
       //code to set clicked image as main images and set alt tag
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", thumbnail.alt);
 
-      //code to set thumbnail as current
-      document.querySelector(".current").classList.remove("current");
-      thumbnail.parentNode.classList.add("current");
+      //code to change which thumbnail is current
+      let currentClass = "current";
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      //code to change image info
+      let title = document.querySelector(".gallery-info > .title");
+      let description = document.querySelector(".gallery-info > .description");
+
+      let newImageTitle = thumbnail.dataset.title;
+      let newImageDescription = thumbnail.dataset.description;
+
+      title.innerHTML = newImageTitle;
+      description.innerHTML = newImageDescription;
     });
   });
 }
